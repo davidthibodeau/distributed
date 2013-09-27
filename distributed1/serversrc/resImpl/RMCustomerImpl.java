a5d861227f0681e5ed5dbb8aa1cacced6aa06e7b
David Thibodeau <david.thibodeau@mail.mcgill.ca>
2013-09-27 17:24:24 -0400

    Added main method for RMCustomerImpl to start the RM. Modified the RM to make it more uniform with the other ones.

--- distributed1/serversrc/resImpl/RMCustomerImpl.java
+++ distributed1/serversrc/resImpl/RMCustomerImpl.java
@@ -1,79 +1,175 @@
 package serversrc.resImpl;
 
+import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 import serversrc.resInterface.*;
 
+import java.util.Calendar;
 import java.util.Enumeration;
 
 
-public class RMCustomerImpl implements RMCustomer{
+public class RMCustomerImpl extends RMBase implements RMCustomer{
 	
-	static private Integer counter = 0;
-	private RMHashtable customers = new RMHashtable();
-	private ResourceManager rm = null;
+	public static void main(String args[]) {
+        // Figure out where server is running
+        String server = "localhost";
+        int port = 1099;
+
+        if (args.length == 1) {
+            server = server + ":" + args[0];
+            port = Integer.parseInt(args[0]);
+        } else if (args.length != 0 &&  args.length != 1) {
+            System.err.println ("Wrong usage");
+            System.out.println("Usage: java ResImpl.ResourceManagerImpl [port]");
+            System.exit(1);
+        }
+
+        try {
+            // create a new Server object
+            RMCustomerImpl obj = new RMCustomerImpl();
+            // dynamically generate the stub (client proxy)
+            RMCustomer rm = (RMCustomer) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry(port);
+            registry.rebind("Group2RMCustomer", rm);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+
+        // Create and install a security manager
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new RMISecurityManager());
+        }
+    }
 	
+	public RMCustomerImpl() throws RemoteException {
+		
+	}
+	
+	private RMItem readData( int id, String key )
+    {
+        synchronized(m_itemHT) {
+            return (RMItem) m_itemHT.get(key);
+        }
+    }
+
+    // Writes a data item
+    private void writeData( int id, String key, RMItem value )
+    {
+        synchronized(m_itemHT) {
+            m_itemHT.put(key, value);
+        }
+    }
 	
 	@Override
 	public int newCustomer(int id) throws RemoteException {
-		int cid = counter.intValue();
-		counter++;
-		Customer customer = new Customer(cid);
-		customers.put(cid, customer);
-		return cid;
+		
+		Trace.info("INFO: RM::newCustomer(" + id + ") called" );
+        // Generate a globally unique ID for the new customer
+        int cid = Integer.parseInt( String.valueOf(id) +
+                                String.valueOf(Calendar.getInstance().get(Calendar.MILLISECOND)) +
+                                String.valueOf( Math.round( Math.random() * 100 + 1 )));
+        Customer cust = new Customer( cid );
+        writeData( id, cust.getKey(), cust );
+        Trace.info("RM::newCustomer(" + cid + ") returns ID=" + cid );
+        return cid;
 	}
+	
 	@Override
 	public boolean newCustomer(int id, int cid) throws RemoteException {
 	    {
-	        Trace.info("RM::addCustomer(" + cid +") called" );
-	        Customer curObj = (Customer) getCustomer( cid );
-	        if ( curObj == null ) {
-	            // doesn't exist...add it
-	            Customer newObj = new Customer( cid);
-	            customers.put(cid, newObj);
-	            Trace.info("RM::addCustomer(" + cid + ") created new customer " + id);
+	    	Trace.info("INFO: RM::newCustomer(" + id + ", " + cid + ") called" );
+	        Customer cust = (Customer) readData( id, Customer.getKey(cid) );
+	        if ( cust == null ) {
+	            cust = new Customer(cid);
+	            writeData( id, cust.getKey(), cust );
+	            Trace.info("INFO: RM::newCustomer(" + id + ", " + cid + ") created a new customer" );
+	            return true;
 	        } else {
-	            Trace.error("RM::addCustomer(" + cid +") Customer already exists, no action taken.");
+	            Trace.info("INFO: RM::newCustomer(" + id + ", " + cid + ") failed--customer already exists");
 	            return false;
-	        }
-	        return(true);
-	    }
+	        } // else
+		}
 	}
 
 	@Override
 	/**
 	 * Must remove all reserved items of that customer as well. 
 	 */
-	public boolean deleteCustomer(int id, int customer) throws RemoteException {
-        Customer curObj = (Customer) getCustomer(id);
-		if ( curObj == null ) {
-            
-            Trace.error("RM::deleteCustomer("+ id+") Customer not found, no action taken.");
+	public boolean deleteCustomer(int id, int customerID) throws RemoteException {
+		
+		Trace.info("RM::deleteCustomer(" + id + ", " + customerID + ") called" );
+        Customer cust = (Customer) readData( id, Customer.getKey(customerID) );
+        if ( cust == null ) {
+            Trace.warn("RM::deleteCustomer(" + id + ", " + customerID + ") failed--customer doesn't exist" );
             return false;
+        } else {            
+            // Increase the reserved numbers of all reservable items which the customer reserved. 
+            RMHashtable reservationHT = cust.getReservations();
+            for (Enumeration e = reservationHT.keys(); e.hasMoreElements();) {        
+                String reservedkey = (String) (e.nextElement());
+                ReservedItem reserveditem = cust.getReservedItem(reservedkey);
+                Trace.info("RM::deleteCustomer(" + id + ", " + customerID + ") has reserved " + reserveditem.getKey() + " " +  reserveditem.getCount() +  " times"  );
+                //TODO: Need to readData in particular RM through middleware
+                ReservableItem item  = (ReservableItem) readData(id, reserveditem.getKey());
+                Trace.info("RM::deleteCustomer(" + id + ", " + customerID + ") has reserved " + reserveditem.getKey() + "which is reserved" +  item.getReserved() +  " times and is still available " + item.getCount() + " times"  );
+                item.setReserved(item.getReserved()-reserveditem.getCount());
+                item.setCount(item.getCount()+reserveditem.getCount());
+            }
+            
+            // remove the customer from the storage
+            removeData(id, cust.getKey());
+            
+            Trace.info("RM::deleteCustomer(" + id + ", " + customerID + ") succeeded" );
+            return true;
+        } // if
+
+	}
+
+    // Returns data structure containing customer reservation info. Returns null if the
+    //  customer doesn't exist. Returns empty RMHashtable if customer exists but has no
+    //  reservations.
+    public RMHashtable getCustomerReservations(int id, int customerID)
+        throws RemoteException
+    {
+        Trace.info("RM::getCustomerReservations(" + id + ", " + customerID + ") called" );
+        Customer cust = (Customer) readData( id, Customer.getKey(customerID) );
+        if ( cust == null ) {
+            Trace.warn("RM::getCustomerReservations failed(" + id + ", " + customerID + ") failed--customer doesn't exist" );
+            return null;
         } else {
-        	RMHashtable reservedItems = curObj.getReservations();
-        	Object key = null;
-        	for (Enumeration e = reservedItems.keys(); e.hasMoreElements(); ) {
-				key = e.nextElement();
-				ReservableItem item = (ReservableItem) reservedItems.get( key );
-				//TODO: Delete all instances of the reservedItem, Suspect we need to use the toString(). 
-			}
-        	customers.remove(id);
-        	Trace.info("RM::deleteCustomer(" + id + "), Customer deleted.");
-        	return true;
-        	
-        }
+            return cust.getReservations();
+        } // if
+    }
+
+	public Customer getCustomer(int id, int customerID){
+		return (Customer) readData( id, Customer.getKey(customerID) );
 	}
 
-
-	public Object getCustomer(Integer id){
-		return customers.get(id);
-	}
-
-	public String queryCustomerInfo(int id, int customer)
-			throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    // return a bill
+    public String queryCustomerInfo(int id, int customerID)
+        throws RemoteException
+    {
+        Trace.info("RM::queryCustomerInfo(" + id + ", " + customerID + ") called" );
+        Customer cust = (Customer) readData( id, Customer.getKey(customerID) );
+        if ( cust == null ) {
+            Trace.warn("RM::queryCustomerInfo(" + id + ", " + customerID + ") failed--customer doesn't exist" );
+            return "";   // NOTE: don't change this--WC counts on this value indicating a customer does not exist...
+        } else {
+                String s = cust.printBill();
+                Trace.info("RM::queryCustomerInfo(" + id + ", " + customerID + "), bill follows..." );
+                System.out.println( s );
+                return s;
+        } // if
+    }
 
 
 
