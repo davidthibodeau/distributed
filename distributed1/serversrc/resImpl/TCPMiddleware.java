b74c17fb644616eaad7e11f986ae9d315528c6dc
jwiener88 <jason.wiener@mail.mcgill.ca>
2013-10-01 16:09:22 -0400

    debug1

--- distributed1/serversrc/resImpl/TCPMiddleware.java
+++ distributed1/serversrc/resImpl/TCPMiddleware.java
@@ -13,8 +13,6 @@ import serversrc.resInterface.*;
 
 @SuppressWarnings("rawtypes")
 public class TCPMiddleware implements Runnable {
-	// TODO: Put the middleware, customer, flight, hotel, cars, and customer on
-	// different ports
 
 	Socket flightSocket;
 	Socket carsSocket;
@@ -40,7 +38,9 @@ public class TCPMiddleware implements Runnable {
 	String server;
 	int port;
 
-	private TCPMiddleware(Socket flightSocket, Socket carsSocket, Socket hotelSocket, Socket customerSocket, Socket clientSocket, String server, int port) {
+	private TCPMiddleware(Socket flightSocket, Socket carsSocket,
+			Socket hotelSocket, Socket customerSocket, Socket clientSocket,
+			String server, int port) {
 		this.server = server;
 		this.port = port;
 		this.clientSocket = clientSocket;
@@ -79,9 +79,9 @@ public class TCPMiddleware implements Runnable {
 				Socket flightSocket = new Socket(args[1], port);
 				Socket hotelSocket = new Socket(args[2], port);
 				Socket customerSocket = new Socket(args[3], port);
-				
-				TCPMiddleware obj = new TCPMiddleware(flightSocket, carsSocket, hotelSocket, customerSocket, clientSocket, server,
-						port);
+
+				TCPMiddleware obj = new TCPMiddleware(flightSocket, carsSocket,
+						hotelSocket, customerSocket, clientSocket, server, port);
 				Thread t = new Thread(obj);
 				t.start();
 
@@ -89,12 +89,10 @@ public class TCPMiddleware implements Runnable {
 		} catch (Exception e) {
 			System.err.println("Connection issue");
 			e.printStackTrace();
-		}
-		finally{
+		} finally {
 			try {
 				connection.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -103,8 +101,7 @@ public class TCPMiddleware implements Runnable {
 	@Override
 	public void run() {
 		try {
-		    System.out.println("Thread started for connection.");
-
+			System.out.println("Thread started for connection.");
 
 			clientOut = new ObjectOutputStream(clientSocket.getOutputStream());
 			clientIn = new ObjectInputStream(clientSocket.getInputStream());
@@ -114,24 +111,22 @@ public class TCPMiddleware implements Runnable {
 
 			carsOut = new ObjectOutputStream(carsSocket.getOutputStream());
 			carsIn = new ObjectInputStream(carsSocket.getInputStream());
-	
+
 			hotelOut = new ObjectOutputStream(hotelSocket.getOutputStream());
 			hotelIn = new ObjectInputStream(hotelSocket.getInputStream());
-	
+
 			customersOut = new ObjectOutputStream(
 					customerSocket.getOutputStream());
 			customersIn = new ObjectInputStream(customerSocket.getInputStream());
-			
-
 
 			Vector methodInvocation;
-			System.out.println("Waiting for query.");
-			while (true){
-			    methodInvocation = (Vector) clientIn.readObject();
-			    if (methodInvocation != null) {
-				System.out.println("Query received");
-				methodSelector(methodInvocation);
-			    }
+			while (true) {
+				System.out.println("Waiting for query.");
+				methodInvocation = (Vector) clientIn.readObject();
+				if (methodInvocation != null) {
+					System.out.println("Query received: " + (String) methodInvocation.elementAt(0));
+					methodSelector(methodInvocation);
+				}
 			}
 		}
 
@@ -149,59 +144,60 @@ public class TCPMiddleware implements Runnable {
 	 * @throws RemoteException
 	 * @throws NumberFormatException
 	 */
-	@SuppressWarnings("unchecked")
 	private void methodSelector(Vector methodInvocation) throws Exception {
 		Vector args = methodInvocation;
 		String method = getString(args.elementAt(0));
-		
-		if (!method.contains("reserve")) {  
-		    if (method.contains("flight")) {
-			// send method to flight manager and get response
-			try {
-			    flightOut.writeObject(methodInvocation);
-			    if (method.contains("new") || method.contains("delete"))
-				clientOut.writeBoolean(flightIn.readBoolean());
-			    else
-					clientOut.writeInt(flightIn.readInt());
-			} catch (IOException e) {
-			    Trace.error("IOException in method invocation: "
-					+ getString(method));
-			    return;
-			}
-			
-			// return flight manager's response return;
-		    }
-		    if (method.contains("car")) {
-			try {
-			    carsOut.writeObject(methodInvocation);
-			    if (method.contains("new") || method.contains("delete")){
-				System.out.println("Waiting for answer");
-				boolean output = carsIn.readBoolean();
-				System.out.println("RM car returned " + output);
-				clientOut.writeBoolean(output);
-			    } else {
-				System.out.println("reached else branch");
-				clientOut.writeInt(carsIn.readInt());
-			    }
-			} catch (IOException e) {
-			    Trace.error("IOException in method invocation: "
-					+ getString(method));
-			}
-		    }
-		    if (method.contains("room")) {
-			try {
-			    hotelOut.writeObject(methodInvocation);
-			    if (method.contains("new") || method.contains("delete"))
-				clientOut.writeBoolean(hotelIn.readBoolean());
-			    else
-				clientOut.writeInt(hotelIn.readInt());
-			} catch (IOException e) {
-			    Trace.error("IOException in method invocation: "
+
+		if (!method.toLowerCase().contains("reserve")) {
+			if (method.contains("flight")) {
+				// send method to flight manager and get response
+				try {
+					flightOut.writeObject(methodInvocation);
+					if (method.contains("new") || method.contains("delete"))
+						clientOut.writeBoolean(flightIn.readBoolean());
+					else
+						clientOut.writeInt(flightIn.readInt());
+				} catch (IOException e) {
+					Trace.error("IOException in method invocation: "
 							+ getString(method));
+					
+				}
+				return;
+				// return flight manager's response return;
 			}
-		    }
-		    if (method.contains("customer")) {
-			
+			if (method.contains("car")) {
+				try {
+					carsOut.writeObject(methodInvocation);
+					if (method.contains("new") || method.contains("delete")) {
+						System.out.println("Waiting for answer");
+						boolean output = carsIn.readBoolean();
+						System.out.println("RM car returned " + output);
+						clientOut.writeBoolean(output);
+					} else {
+						System.out.println("reached else branch");
+						clientOut.writeInt(carsIn.readInt());
+					}
+				} catch (IOException e) {
+					Trace.error("IOException in method invocation: "
+							+ getString(method));
+				}
+				return;
+			}
+			if (method.contains("room")) {
+				try {
+					hotelOut.writeObject(methodInvocation);
+					if (method.contains("new") || method.contains("delete"))
+						clientOut.writeBoolean(hotelIn.readBoolean());
+					else
+						clientOut.writeInt(hotelIn.readInt());
+				} catch (IOException e) {
+					Trace.error("IOException in method invocation: "
+							+ getString(method));
+				}
+				return;
+			}
+			if (method.contains("customer")) {
+
 				try {
 					customersOut.writeObject(methodInvocation);
 					if (method.equalsIgnoreCase("deletecustomer")) {
@@ -227,25 +223,21 @@ public class TCPMiddleware implements Runnable {
 								hotelOut.writeObject(unreserve);
 								success = flightIn.readBoolean();
 							}
-							
+
 							clientOut.writeBoolean(success);
-							return;
 						}
-					}
-					else if (method.equalsIgnoreCase("newCustomer")){
-						if (args.size() == 3) 
+					} else if (method.equalsIgnoreCase("newCustomer")) {
+						if (args.size() == 3)
 							clientOut.writeInt(clientIn.readInt());
 						else
 							clientOut.writeBoolean(clientIn.readBoolean());
 						return;
-					}
-					else if (method.equalsIgnoreCase("getCustomer")){
+					} else if (method.equalsIgnoreCase("getCustomer")) {
 						clientOut.writeObject(customersIn.readObject());
 						return;
-					}
-					else
+					} else
 						clientOut.writeObject(customersIn.readObject());
-					
+
 				} catch (IOException e) {
 					Trace.error("IOException in method invocation: "
 							+ getString(method));
@@ -275,24 +267,24 @@ public class TCPMiddleware implements Runnable {
 	public boolean reserveFlight(int id, int customer, int flightNum)
 			throws IOException {
 
-		 ReservedItem item = reserveItem(id, customer, Flight.getKey(flightNum),
+		ReservedItem item = reserveItem(id, customer, Flight.getKey(flightNum),
 				String.valueOf(flightNum), ReservedItem.rType.FLIGHT);
-		 return (item != null);
+		return (item != null);
 	}
 
 	public boolean reserveCar(int id, int customer, String location)
 			throws IOException {
 
-		ReservedItem item = reserveItem(id, customer, Car.getKey(location), location,
-				ReservedItem.rType.CAR);
+		ReservedItem item = reserveItem(id, customer, Car.getKey(location),
+				location, ReservedItem.rType.CAR);
 		return (item != null);
 	}
 
 	public boolean reserveRoom(int id, int customer, String location)
 			throws IOException {
 
-		ReservedItem item = reserveItem(id, customer, Hotel.getKey(location), location,
-				ReservedItem.rType.ROOM);
+		ReservedItem item = reserveItem(id, customer, Hotel.getKey(location),
+				location, ReservedItem.rType.ROOM);
 		return (item != null);
 	}
 
@@ -302,76 +294,104 @@ public class TCPMiddleware implements Runnable {
 				+ flightNumbers + ", " + location + ", " + car + ", " + room
 				+ " ) called");
 		// Read customer object if it exists (and read lock it)
-        Customer cust = null;
-        Vector<Object> args = new Vector<Object>();
+		Customer cust = null;
+		Vector<Object> args = new Vector<Object>();
 		args.add("getCustomer");
 		args.add(id);
 		args.add(customer);
-		
+
 		customersOut.writeObject(args);
-        try {
+		try {
 			cust = (Customer) customersIn.readObject();
 		} catch (ClassNotFoundException e) {
 			Trace.error("Object returned was not a Customer ");
 			return false;
 		}
-        if ( cust == null ) {
-        	Trace.info("RM::itinerary( " + id + ", customer=" + customer + ", " +flightNumbers+ ", "+location+
-					", " + car + ", " + room + " ) -- Customer non existent." );
-        	return false;
-        }
-        ReservedItem reservedCar = null;
-        ReservedItem reservedRoom = null;
-        if (car){
-        	reservedCar = reserveItem(id, customer, Car.getKey(location) ,location, ReservedItem.rType.CAR);
-        	if (reservedCar == null) {
-        		Trace.info("RM::itinerary( " + id + ", customer=" + customer + ", " +flightNumbers+ ", "+location+
-    					", " + car + ", " + room + " ) -- Car could not have been reserved." );
-        		return false;
-        	}
-        }
-        if (room){
-        	reservedRoom = reserveItem(id, customer, Hotel.getKey(location) ,location, ReservedItem.rType.ROOM);
-        	if (reservedRoom == null) {
-        		Trace.info("RM::itinerary( " + id + ", customer=" + customer + ", " +flightNumbers+ ", "+location+
-    					", " + car + ", " + room + " ) -- Room could not have been reserved." );
-        		unreserveItem(id, customer, reservedCar, ReservedItem.rType.CAR);
-        		return false;
-        	}
-        }
-        Vector flightsDone = new Vector();
-        for (Enumeration e = flightNumbers.elements(); e.hasMoreElements();) {
-        	int flightnum = 0;
-        	try {
-        		flightnum = getInt(e.nextElement());
-        	} catch(Exception ex) {
-        		Trace.info("RM::itinerary( " + id + ", customer=" + customer + ", " +flightNumbers+ ", "+location+
-    					", " + car + ", " + room + " ) -- Expected FlightNumber was not a valid integer. Exception "
-    					+ ex + " cached");
-        		unreserveItem(id, customer, reservedCar, ReservedItem.rType.CAR);
-        		unreserveItem(id, customer, reservedRoom, ReservedItem.rType.ROOM);
-        		for (Enumeration f = flightsDone.elements(); f.hasMoreElements();) {
-        			unreserveItem(id, customer, (ReservedItem) f.nextElement(), ReservedItem.rType.ROOM);
-        		}
-        		return false;
-        	}
-        	ReservedItem reservedFlight = reserveItem(id, customer, Flight.getKey(flightnum), String.valueOf(flightnum), ReservedItem.rType.FLIGHT);
-        	if (reservedFlight == null){
-        		Trace.info("RM::itinerary( " + id + ", customer=" + customer + ", " +flightnum+ ", "+location+
-    					", " + car + ", " + room + " ) -- flight could not have been reserved." );
-        		unreserveItem(id, customer, reservedCar, ReservedItem.rType.CAR);
-        		unreserveItem(id, customer, reservedRoom, ReservedItem.rType.ROOM);
-        		for (Enumeration f = flightsDone.elements(); f.hasMoreElements();) {
-        			unreserveItem(id, customer, (ReservedItem) f.nextElement(), ReservedItem.rType.ROOM);
-        		}
-        		return false;
-        	}
-        	flightsDone.add(reservedFlight);	
-        }
-        	
+		if (cust == null) {
+			Trace.info("RM::itinerary( " + id + ", customer=" + customer + ", "
+					+ flightNumbers + ", " + location + ", " + car + ", "
+					+ room + " ) -- Customer non existent.");
+			return false;
+		}
+		ReservedItem reservedCar = null;
+		ReservedItem reservedRoom = null;
+		if (car) {
+			reservedCar = reserveItem(id, customer, Car.getKey(location),
+					location, ReservedItem.rType.CAR);
+			if (reservedCar == null) {
+				Trace.info("RM::itinerary( " + id + ", customer=" + customer
+						+ ", " + flightNumbers + ", " + location + ", " + car
+						+ ", " + room
+						+ " ) -- Car could not have been reserved.");
+				return false;
+			}
+		}
+		if (room) {
+			reservedRoom = reserveItem(id, customer, Hotel.getKey(location),
+					location, ReservedItem.rType.ROOM);
+			if (reservedRoom == null) {
+				Trace.info("RM::itinerary( " + id + ", customer=" + customer
+						+ ", " + flightNumbers + ", " + location + ", " + car
+						+ ", " + room
+						+ " ) -- Room could not have been reserved.");
+				unreserveItem(id, customer, reservedCar, ReservedItem.rType.CAR);
+				return false;
+			}
+		}
+		Vector flightsDone = new Vector();
+		for (Enumeration e = flightNumbers.elements(); e.hasMoreElements();) {
+			int flightnum = 0;
+			try {
+				flightnum = getInt(e.nextElement());
+			} catch (Exception ex) {
+				Trace.info("RM::itinerary( "
+						+ id
+						+ ", customer="
+						+ customer
+						+ ", "
+						+ flightNumbers
+						+ ", "
+						+ location
+						+ ", "
+						+ car
+						+ ", "
+						+ room
+						+ " ) -- Expected FlightNumber was not a valid integer. Exception "
+						+ ex + " cached");
+				unreserveItem(id, customer, reservedCar, ReservedItem.rType.CAR);
+				unreserveItem(id, customer, reservedRoom,
+						ReservedItem.rType.ROOM);
+				for (Enumeration f = flightsDone.elements(); f
+						.hasMoreElements();) {
+					unreserveItem(id, customer, (ReservedItem) f.nextElement(),
+							ReservedItem.rType.ROOM);
+				}
+				return false;
+			}
+			ReservedItem reservedFlight = reserveItem(id, customer,
+					Flight.getKey(flightnum), String.valueOf(flightnum),
+					ReservedItem.rType.FLIGHT);
+			if (reservedFlight == null) {
+				Trace.info("RM::itinerary( " + id + ", customer=" + customer
+						+ ", " + flightnum + ", " + location + ", " + car
+						+ ", " + room
+						+ " ) -- flight could not have been reserved.");
+				unreserveItem(id, customer, reservedCar, ReservedItem.rType.CAR);
+				unreserveItem(id, customer, reservedRoom,
+						ReservedItem.rType.ROOM);
+				for (Enumeration f = flightsDone.elements(); f
+						.hasMoreElements();) {
+					unreserveItem(id, customer, (ReservedItem) f.nextElement(),
+							ReservedItem.rType.ROOM);
+				}
+				return false;
+			}
+			flightsDone.add(reservedFlight);
+		}
+
 		return true;
 	}
-	
+
 	/*
 	 * Call RMCust to obtain customer, if it exists. Verify if item exists and
 	 * is available. (Call RM*obj*) Reserve with RMCustomer Tell RM*obj* to
@@ -431,7 +451,7 @@ public class TCPMiddleware implements Runnable {
 			customersOut.writeObject(args);
 			try {
 				ReservedItem result = (ReservedItem) customersIn.readObject();
-				if (result == null){
+				if (result == null) {
 					args.set(0, "unreserveKey");
 					args.set(2, key);
 					if (rtype == ReservedItem.rType.CAR) {
@@ -453,12 +473,15 @@ public class TCPMiddleware implements Runnable {
 			}
 		}
 	}
-	
+
 	/*
-	 * unreserveItem is used by the itinerary class to cancel a reserved item when the whole reservation failed.
+	 * unreserveItem is used by the itinerary class to cancel a reserved item
+	 * when the whole reservation failed.
 	 */
-	protected boolean unreserveItem(int id, int customerID, ReservedItem item, ReservedItem.rType rtype) {
-		Trace.info("RM::unreserveItem( " + id + ", customer=" + customerID + ", " +item+ " ) called" );        
+	protected boolean unreserveItem(int id, int customerID, ReservedItem item,
+			ReservedItem.rType rtype) {
+		Trace.info("RM::unreserveItem( " + id + ", customer=" + customerID
+				+ ", " + item + " ) called");
 		// Verifies if customer exists
 		Vector<Object> args = new Vector<Object>();
 		args.add("unreserve");
@@ -467,34 +490,39 @@ public class TCPMiddleware implements Runnable {
 		args.add(item);
 		try {
 			customersOut.writeObject(args);
-			Customer cust = (Customer) customersIn.readObject() ;
-			if(cust == null){
-				Trace.warn("RM::unreserveItem( " + id + ", " + customerID + ", " +item +" ) failed -- Customer has been deleted." );
+			Customer cust = (Customer) customersIn.readObject();
+			if (cust == null) {
+				Trace.warn("RM::unreserveItem( " + id + ", " + customerID
+						+ ", " + item
+						+ " ) failed -- Customer has been deleted.");
 				return false;
 			}
-		} catch (Exception e){
-			Trace.error("RM::unreserveItem( " + id + ", " + customerID + ", " +item +" ) failed -- IOException." );
+		} catch (Exception e) {
+			Trace.error("RM::unreserveItem( " + id + ", " + customerID + ", "
+					+ item + " ) failed -- IOException.");
 			return false;
 		}
 
 		boolean done = false;
 		try {
-		if (rtype == ReservedItem.rType.CAR) {
-			carsOut.writeObject(args);
-			done = carsIn.readBoolean();
-		} else if (rtype == ReservedItem.rType.FLIGHT) {
-			flightOut.writeObject(args);
-			done = flightIn.readBoolean();
-		} else if (rtype == ReservedItem.rType.ROOM) {
-			hotelOut.writeObject(args);
-			done = hotelIn.readBoolean();
-		}
+			if (rtype == ReservedItem.rType.CAR) {
+				carsOut.writeObject(args);
+				done = carsIn.readBoolean();
+			} else if (rtype == ReservedItem.rType.FLIGHT) {
+				flightOut.writeObject(args);
+				done = flightIn.readBoolean();
+			} else if (rtype == ReservedItem.rType.ROOM) {
+				hotelOut.writeObject(args);
+				done = hotelIn.readBoolean();
+			}
 		} catch (IOException e) {
-			Trace.error("RM::unreserveItem( " + id + ", " + customerID + ", " +item +" ) failed -- IOException." );
+			Trace.error("RM::unreserveItem( " + id + ", " + customerID + ", "
+					+ item + " ) failed -- IOException.");
 			return false;
 		}
 		if (!done) {
-			Trace.warn("RM::unreserveItem( " + id + ", " + customerID + ", " +item +" ) failed-- Object RM returned false." );
+			Trace.warn("RM::unreserveItem( " + id + ", " + customerID + ", "
+					+ item + " ) failed-- Object RM returned false.");
 			return false;
 		}
 		return true;
